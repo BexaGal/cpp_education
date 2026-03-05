6cbe23f9f68956d87abfa8f96f13758fda37df07
Bakhtiyar Galimov <bakhtiyar.galimov@umbrellait.com>
2026-03-05 13:46:19 +0300

    Quadrant test

--- 05_03_2026/1.cpp
+++ 05_03_2026/1.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+ int main (int argc, char* argv[]){
+    int x, y;
+    int map[4][2] = {{1, 1}, {-1,1}, {-1,-1}, {1,-1}};
+    cin >> x; cin >> y;
+    if (x == 0){ cout << "On line X" << endl; return 0;}
+    if (y == 0){ cout << "On line Y" << endl; return 0;}
+    for (int i = 0; i < 4; i++){
+        if (x * map[i][0] > 0 && y * map[i][1] > 0){
+            cout << "Quadrant " << i + 1 << endl;
+            break;
+        }
+    }
+
+    return 0;
+ }
